ee8acd540478c3fad35ebdc8edf6b6013ff625a8
Arindam Roy <rarindam@gmail.com>
2022-05-09 17:49:09 -0700

    Update for latest bootstrap and EJS
    Updated some of the files and references to
    bootstrap and modified EJS for errors while
    rendering messages.
    
    Signed-off-by: Arindam Roy <rarindam@gmail.com>

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+//dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb://192.168.0.17/youtuberegistration';
 
 module.exports = {
     mongoURI: dbPassword
